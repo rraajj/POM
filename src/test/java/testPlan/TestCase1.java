d878e4c47fb3323ab8d9b8d44295942a7ae0ddfd
Rajesh <coolrajesh11@gmail.com>
2022-04-17 18:27:35 +0530

    POM FILE

---
+++ src/test/java/testPlan/TestCase1.java
@@ -0,0 +1,54 @@
+package testPlan;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestCase1 {
+
+	public static WebDriver driver;
+	//public WebElement googleSearch = (WebElement) By.xpath("//*[@name='id']");
+
+	@Test
+	public void launchBrowser() throws  IOException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("http://google.co.in");
+		driver.findElement(By.xpath("//*[@name='q']")).sendKeys("Rajesh Thangavelu");
+		driver.findElement(By.xpath("//input[@value='Google Search']")).click();
+		//googleSearch.sendKeys("Rajesh Thangavelu");
+		
+		File srcshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		File desshot = new File("C:\\Users\\NilavezhilRajesh\\eclipse-workspace\\POM\\src\\test\\resources\\screenshot\\test.png");
+		FileUtils.copyFile(srcshot, desshot);
+		 
+		 
+
+		// *[@name='id']
+		//Thread.sleep(1000);
+		//driver.quit();
+		driver.quit();
+        
+	}
+
+	@AfterMethod
+	public void tearDown(WebDriver driver) 
+	{
+       driver.quit();
+		 
+	}
+
+}
